d1bf4a5e3e3a4da37ec315fa5075ee2666a59a47
Topher-06 <christopherfrancisco171@gmail.com>
2022-04-16 14:25:31 +0800

    new

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,24 @@
 <script>
-	export let name;
+	import Router from "svelte-spa-router";
+	import Nav from "./pages/Nav.svelte";
+
+	import Home from "./pages/Home.svelte";
+	import Action from "./pages/Action.svelte";
+	import Efficacy from "./pages/Efficacy.svelte";
+	import Bioavailability from "./pages/Bioavailability.svelte";
+	import Covid19 from "./pages/Covid19.svelte";
+	import Dosage from "./pages/Dosage.svelte";
+
+	const routes = {
+		"/": Home,
+		"/action": Action,
+		"/efficacy": Efficacy,
+		"/bio": Bioavailability,
+		"/covid19": Covid19,
+		"/dosage": Dosage,
+		// '*': NotFound
+	};
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router {routes} />
+<Nav />
---
+++ src/pages/Nav.svelte
@@ -0,0 +1,82 @@
+<nav class="navbar navbar-expand-lg navigation">
+    <img src="assets/nav/BAR-LOGO.png" style="width: 18%;" alt="" />
+    <a href="#/">
+    <button class="button btn-sm flex-grow-1 ml-3">
+        <img
+            src="assets/nav/clicked/BUTTON-HOME_CLICKED.png"
+            style="width: 100%; margin-top:-10%;"
+            alt=""
+        />
+    </button>
+</a>
+<a href="#/action">
+    <button class="btn-sm flex-grow-1">
+        <img
+            src="assets/nav/default/BUTTON-ACTION_DEFAULT.png"
+            style="width: 100%;"
+            alt=""
+        />
+    </button>
+    </a>
+    <a href="#/efficacy">
+    <button class="btn-sm flex-grow-1">
+        <img
+            src="assets/nav/default/BUTTON-EFFICACY_DEFAULT.png"
+            style="width: 100%;"
+            alt=""
+        />
+    </button>
+    </a>
+    <a href="#/bio">
+    <button class="btn-sm flex-grow-1">
+        <img
+            src="assets/nav/default/BUTTON-BIOAVAILABILITY_DEFAULT.png"
+            style="width: 100%;"
+            alt=""
+        />
+    </button>
+    </a>
+    <a href="#/covid19">
+    <button class="btn-sm flex-grow-1">
+        <img
+            src="assets/nav/default/BUTTON-COVID19_DEFAULT.png"
+            style="width: 100%;"
+            alt=""
+        />
+    </button>
+    </a>
+    <a href="#/dosage">
+    <button class="btn-sm flex-grow-1">
+        <img
+            src="assets/nav/default/BUTTON-DOSAGE_DEFAULT.png"
+            style="width: 100%;"
+            alt=""
+        />
+    </button>
+    </a>
+    <a href="ex">
+    <button class="btn-sm flex-grow-1">
+        <img
+            src="assets/nav/default/BUTTON-EXIT_DEFAULT.png"
+            style="width: 100%;"
+            alt=""
+        />
+    </button>
+    </a>
+</nav>
+
+<style>
+    .navigation {
+        position: absolute;
+        bottom: 0;
+    }
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+    *:focus {
+        outline: 0 !important;
+        background: none;
+    }
+</style>
